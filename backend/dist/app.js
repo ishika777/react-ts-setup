123fd553913275ec57af0e0da501949c0b980ac0
Ishika Kansal <ishikakansal05@gmail.com>
2026-02-06 09:26:47 +0530

    added all files with .env

---
+++ backend/dist/app.js
@@ -0,0 +1,10 @@
+import express from "express";
+import cors from "cors";
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.get("/", (_req, res) => {
+    res.send("Backend is alive 🚀");
+});
+export default app;
+//# sourceMappingURL=app.js.map
---
+++ backend/src/server.ts
@@ -0,0 +1,23 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv"
+
+dotenv.config({debug: true});
+
+
+//  add .gitignore in backend folder 
+
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (_req, res) => {
+  res.send("Backend is alive 🚀");
+});
+
+app.listen(port, () => {
+    console.log(`server listening on port ${port}`)
+})
